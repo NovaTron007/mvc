006560bd926a21b640a8f45339d93880ac3171be
NovaTron007 <dwayne.man@gmail.com>
2021-01-17 20:42:23 +0800

    Getting the url when entered. Stripping url at slashes and puttting values into array using explode method. Loading default Pages controller.

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -11,11 +11,34 @@ class Core {
   protected $params = [];
  
   public function __construct(){
-    $this->getUrl();
+    // print_r($this->getUrl());
+    $url = $this->getUrl();
+
+    // Get controler: look in controllers for first value of url. 
+    // Everything routed to index so we set path from inside there to get to app folder
+    // gets val in $url[0] so 'post' ie: http://localhost/repo/mvc/post
+    // ucwords to capitalise ie Post as controller methods are capitalised
+    if(file_exists('../app/controllers/' . ucwords($url[0])) . '.php'){ 
+      // if exists, set as controller
+      $this->currentController = ucwords($url[0]);
+      // unset 0 index
+      unset($url[0]);
+    }
+
+    //require the controller
+    require_once('../app/controllers/' . $this->currentController . '.php');
+
+    // instantiate controller ie $pages = new Pages
+    $this->currentController = new $this->currentController;
   }
   
   public function getUrl(){
-    echo $_GET['url'];
+    if(isset($_GET['url'])){ // gets the url
+      $url = rtrim($_GET['url'],'/');// strip what you want (/);
+      $url = filter_var($url, FILTER_SANITIZE_URL);//prevent chars url should not have 
+      $url = explode('/', $url); // break url into array at slashes ie /post/edit/1 (post,edit,1)
+      return $url;
+    }
   }
 
 }
